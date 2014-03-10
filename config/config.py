560d775b59bdccd35444a32740364c343dd6ad60
Jon Morehouse <morehousej09@gmail.com>
2014-03-10 01:00:53 -0700

    Begin initializing project and focusing on simply configuration

--- config/config.py
+++ config/config.py
@@ -7,25 +7,54 @@ logger = logging.getLogger()
 # set any weird config settings here / defaults as needed
 class Config(object):
 
-	@staticmethod
-	def load(variables):
-		
-		# now for each of the config_variables - set the class attribute
-		# note that we want this to throw errors if the environment variable isn't set
-		for variable in variables:
+  @staticmethod
+  def load(variables):
+    
+    # now for each of the config_variables - set the class attribute
+    # note that we want this to throw errors if the environment variable isn't set
+    for variable in variables:
 
-			# set the attribute on the class 
-			value = environ.get(variable)
+      # set the attribute on the class 
+      value = environ.get(variable)
 
-			# if the value exists - set it as a class attribute
-			if  value:
+      # if the value exists - set it as a class attribute
+      if  value:
 
-				value = setattr(Config, variable, value)
+        value = setattr(Config, variable, value)
 
-			# if we don't have a default attribute in place and no config - then go ahead and throw an error
-			elif not hasattr(Config, variable):
+      # if we don't have a default attribute in place and no config - then go ahead and throw an error
+      elif not hasattr(Config, variable):
 
-				# log a warning here
-				logger.warning("Config variable %s not set." % variable) 
+        # log a warning here
+        logger.warning("Config variable %s not set." % variable) 
+
+  @staticmethod
+  def load_from_file(path): 
+  
+      # first find out the extension of the file as needed 
+      # then loop through and set the various values as needed in different manners
+      pass
+
+
+
+  def _load_exports_file(path):
+
+      pass
+
+
+  def _load_yaml_file(path):
+
+
+      pass
+
+
+  def _load_json_file(path):
+
+      pass
+
+
+
+
+
 
 
--- setup.py
+++ setup.py
@@ -2,49 +2,64 @@
 import os
 import sys
 
-# import utilities version etc
-from utilities import __version__
+with open("config/version", "r") as f:
 
-# import setuptools as needed
+  version = f.read()
+
+# import setuptool requirements
 try:
-	from setuptools import setup
-	from setuptools.command.test import test as TestCommand
-	
+    
+  from setuptools import setup
+  from setuptools.command.test import test as TestCommand
+
 except ImportError:
 
-	from distutils.core import setup
+  from distutils.core import setup
 
-# grab our readme generated rst for this project
-with open("readme.rst", "r") as _file:
 
-	long_description = _file.read()
+# generate readme as needed before moving forward with anything
+with open("readme.rst", "r") as f:
 
-# setup project 
+  long_description = f.read()
+
+# load in all requirements for this project as needed
+with open("requirements.txt", "r") as f:
+  
+  requirements = f.read().splitlines()
+
+with open("test-requirements.txt", "r") as f:
+
+  test_requirements = f.read().splitlines()
+
 setup(
-	
-	name="py-config",
-	version = __version__,
-	description = "General python utilities for projects",
-	long_description = long_description,
-	url = "http://github.com/jonmorehouse/pyconfig",
-	author = "Jon Morehouse",
-	author_email = "morehousej09@gmail.com",
-	keywords = ["py-config"],
-	license = "MIT",
-	packages = ["config"],
-
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.5',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-	]
+    name = "py-config",
+    version = version,
+    description = "Configuration manager for python projects",
+    long_description = long_description,
+    url = "http://github.com/jonmorehouse/py-config",
+    author = "Jon Morehouse",
+    author_email = "morehousej09@gmail.com",
+    keywords = ["configuration", "management"],
+    license = "MIT",
+    packages = ["config"],
+    install_requires = requirements,
+    test_requires = test_requirements,
+    test_suite = "nose.collector",
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+    ]
 )
+
+
+
